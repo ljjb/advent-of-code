828700066430072d78ef4fcd4fa0d7330224522a
Leo Bouillet <leojjbouillet@gmail.com>
2023-12-03 00:35:06 +1100

    (2022) apply formatting

--- 2022/lib/day01.ml
+++ 2022/lib/day01.ml
@@ -3,12 +3,12 @@ let read_chunks file =
     match input_line ic with
     | "" -> Result.Ok acc
     | exception End_of_file ->
-        close_in ic;
-        Result.Error acc
-    | str -> (
-        match int_of_string str with
-        | integer -> read_chunk ic (integer :: acc)
-        | exception Failure _ -> Result.Ok acc)
+      close_in ic;
+      Result.Error acc
+    | str ->
+      (match int_of_string str with
+       | integer -> read_chunk ic (integer :: acc)
+       | exception Failure _ -> Result.Ok acc)
   in
   let rec aux ic =
     match read_chunk ic [] with
@@ -16,16 +16,21 @@ let read_chunks file =
     | Result.Ok chunk -> Seq.Cons (chunk, fun () -> aux ic)
   in
   let ic = open_in file in
-  try fun () -> aux ic
-  with e ->
+  try fun () -> aux ic with
+  | e ->
     close_in_noerr ic;
     raise e
+;;
 
 let top3 (a, b, c) cur =
-  if cur > a then (cur, a, b)
-  else if cur > b then (a, cur, b)
-  else if cur > c then (a, b, cur)
-  else (a, b, c)
+  if cur > a
+  then cur, a, b
+  else if cur > b
+  then a, cur, b
+  else if cur > c
+  then a, b, cur
+  else a, b, c
+;;
 
 let solve filename =
   let a, b, c =
@@ -33,4 +38,5 @@ let solve filename =
     |> Seq.map (List.fold_left ( + ) 0)
     |> Seq.fold_left top3 (0, 0, 0)
   in
-  (a, a + b + c)
+  a, a + b + c
+;;

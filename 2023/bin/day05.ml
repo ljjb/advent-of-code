28031c97c0144cc29cdec02b78d8a1230c21e90d
Leo Bouillet <leojjbouillet@gmail.com>
2023-12-06 00:48:06 +1100

    (2023) add day 05

---
+++ 2023/bin/day05.ml
@@ -0,0 +1,9 @@
+open Advent2023.Day05
+
+let () =
+  let filename = "input/day05.txt" in
+  (* let sol1 = part1 filename in *)
+  (* Format.printf "%s\n" sol1 *)
+  let sol2 = part2 filename in
+  Format.printf "%s\n" sol2
+;;

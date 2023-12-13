509fbd4708d399cf6026a87e2aaaf784e07c07d2
Leo Bouillet <leojjbouillet@gmail.com>
2023-12-13 22:33:52 +1100

    (2023) finish day 09

--- 2023/bin/day09.ml
+++ 2023/bin/day09.ml
@@ -4,5 +4,7 @@ let () =
   let filename = "input/day09.txt" in
   let sol1 = part1 filename in
   print_string sol1;
-  Out_channel.(flush stdout)
+  Out_channel.(flush stdout);
+  let sol2 = part2 filename in
+  Format.printf "%s\n" sol2
 ;;
--- 2023/lib/day09.ml
+++ 2023/lib/day09.ml
@@ -10,7 +10,51 @@ module Parse = struct
   let parse = parse_general puzzle
 end
 
-let part1 filename =
-  let puzzle = In_channel.read_all filename |> Parse.parse in
-  puzzle |> sprintf !"%{sexp:(int list list)}\n"
+let next_level xs =
+  let seq = Sequence.of_list xs in
+  Sequence.zip seq @@ Sequence.drop seq 1
+  (* expecting input in reverse, then reversed again by zip above *)
+  |> Sequence.map ~f:(fun (nxt, prv) -> nxt - prv)
+  |> Sequence.to_list
 ;;
+
+let layers ~dir xs =
+  let xs =
+    match dir with
+    | `Forward -> List.rev xs
+    | `Backward -> xs
+  in
+  let rec aux acc xs =
+    let xs' = next_level xs in
+    if List.for_all xs' ~f:(fun ele -> ele = 0) then xs' :: acc else aux (xs' :: acc) xs'
+  in
+  aux [ xs ] xs
+;;
+
+let extrapolate ?(dir = `Forward) xs =
+  let xs = layers ~dir xs in
+  let rec loop acc prv_ele xs =
+    match xs with
+    | [] -> acc
+    | cur_row :: xs ->
+      let cur_ele = prv_ele + List.hd_exn cur_row in
+      let acc = cur_ele :: acc in
+      loop acc cur_ele xs
+  in
+  match xs with
+  | _ :: _ ->
+    let acc = loop [] 0 xs in
+    List.hd_exn acc
+  | _ -> failwith "nooo!"
+;;
+
+let solve ~dir filename =
+  let puzzle = In_channel.read_all filename |> Parse.parse in
+  puzzle
+  |> List.map ~f:(extrapolate ~dir)
+  |> List.sum (module Int) ~f:Util.id
+  |> sprintf !"%{sexp:(int)}\n"
+;;
+
+let part1 = solve ~dir:`Forward
+let part2 = solve ~dir:`Backward
